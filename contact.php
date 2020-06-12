c1df4885d872e5b3a2926a5de177189efa9495d4
Dana Nobile <danagnobile@gmail.com>
2020-06-12 12:52:15 -0400

    +Testing email again, different php config

--- contact.php
+++ contact.php
@@ -1,15 +1,5 @@
 <?php
-
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $mailFrom = $_POST['email'];
-    $message = $_POST['message'];
-
-    $mailTo = 'danagnobile@gmail.com';
-    $headers = "From: ".$mailFrom;
-    $txt = "You have an email from ".$name.".\n\n".$message;
-
-
-    mail($mailTo, $txt, $headers);
-    header("Location: index.php?mailsend")
+if($_POST["message"]) {
+    mail("danagnobile@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
 }
+?>
